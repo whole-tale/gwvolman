1950320b034d89af8f0b440ff638be31e0aafa1c
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2017-06-15 13:50:49 -0500

    Add requirements

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 from setuptools import setup, find_packages
 
-setup(name='gwvolman',
-      version='0.0.1',
-      description='An example girder worker extension',
-      author='Kacper Kowalik',
-      author_email='xarthisius.kk@gmail.com',
-      license='MIT',
-      classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
-          'License :: OSI Approved :: Apache Software License'
-          'Intended Audience :: Science/Research',
-          'Natural Language :: English',
-          'Programming Language :: Python'
-      ],
-      entry_points={
-          'girder_worker_plugins': [
-              'gwvolman = gwvolman:GWVolumeManagerPlugin',
-          ]
-      },
-      packages=find_packages(),
-      zip_safe=False)
+setup(
+    name='gwvolman',
+    version='0.0.1',
+    description='An example girder worker extension',
+    author='Kacper Kowalik',
+    author_email='xarthisius.kk@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: Apache Software License'
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Programming Language :: Python'
+    ],
+    entry_points={
+        'girder_worker_plugins': [
+            'gwvolman = gwvolman:GWVolumeManagerPlugin',
+        ]
+    },
+    install_requires=[
+        'girder-client>=2.1.0',
+        'docker>=2.3.0',
+        'requests>=2.18.1'
+    ],
+    packages=find_packages(),
+    zip_safe=False
+)
