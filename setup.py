8afa8046fa9a96d123461a3f3e30b09766d2fefd
Craig Willis <willis8@illinois.edu>
2018-06-08 12:55:49 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gwvolman',
-    version='0.2.0',
+    version='0.3.0',
     description='An example girder worker extension',
     author='Kacper Kowalik',
     author_email='xarthisius.kk@gmail.com',
