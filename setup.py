4e0385f06293c137b690edc9603b49ac2e839363
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2019-11-15 08:56:11 -0600

    Bugfix release (v0.8.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gwvolman',
-    version='0.9.0dev0',
+    version='0.8.1',
     description='An example girder worker extension',
     author='Kacper Kowalik',
     author_email='xarthisius.kk@gmail.com',
