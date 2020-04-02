45457a893e3f773a2b757b34ea77c103f4c0186b
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2020-04-02 09:12:26 -0500

    Bump version to 0.9.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gwvolman',
-    version='0.9.0rc3',
+    version='0.9.0',
     description='An example girder worker extension',
     author='Kacper Kowalik',
     author_email='xarthisius.kk@gmail.com',
