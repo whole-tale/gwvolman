fe73ef167bd91a1fa1faaf5aa407b37518136fe8
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2023-04-18 09:31:41 -0500

    Automatically create necessary queues

--- gwvolman/__init__.py
+++ gwvolman/__init__.py
@@ -1,4 +1,6 @@
 """WholeTale Girder Worker Plugin."""
+import os
+import docker
 from girder_worker import GirderWorkerPluginABC
 from kombu.common import Broadcast, Exchange, Queue
 
@@ -12,13 +14,36 @@ class GWVolumeManagerPlugin(GirderWorkerPluginABC):
         # Here we can also change application settings. E.g.
         # changing the task time limit:
         #
-        self.app.conf.task_queues = (
-            Queue('celery', Exchange('celery', type='direct'),
-                  routing_key='celery'),
-            Broadcast('broadcast_tasks')
-        )
+        queues = []
+        if node_id := os.environ.get("SWARM_NODE_ID"):
+            cli = docker.from_env()
+            node = cli.nodes.get(node_id)
+            if node.attrs["Spec"]["Role"] == "manager":
+                queues.append(
+                    Queue("manager", Exchange("manager", type="direct"), routing_key="manager")
+                )
+
+            if len(cli.nodes.list()) == 1 or node.attrs["Spec"]["Role"] != "manager":
+                queues.append(
+                    Queue("celery", Exchange("celery", type="direct"), routing_key="celery")
+                )
+
+            queues += [
+                Queue(node_id, Exchange(node_id, type="direct"), routing_key=node_id),
+            ]
+        else:
+            queues = [
+                Queue(
+                    "celery",
+                    Exchange("celery", type="direct"),
+                    routing_key="celery",
+                ),
+            ]
+        queues.append(Broadcast("broadcast_tasks"))
+
+        self.app.conf.task_queues = queues
         self.app.conf.task_routes = {
-            'gwvolman.tasks.shutdown_container': {'queue': 'broadcast_tasks'}
+            "gwvolman.tasks.shutdown_container": {"queue": "broadcast_tasks"}
         }
         # self.app.config.update({
         #     'TASK_TIME_LIMIT': 300
@@ -26,4 +51,4 @@ class GWVolumeManagerPlugin(GirderWorkerPluginABC):
 
     def task_imports(self):
         """Return a list of python importable paths."""
-        return ['gwvolman.tasks']
+        return ["gwvolman.tasks"]
