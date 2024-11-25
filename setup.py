1dc7e8def569dc40cdf2e8ae00ff58cb5dad7fbd
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2024-11-25 17:24:52 -0600

    Add a pseudo-release, for scm compatibility

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gwvolman",
-    version="1.3.dev0",
+    version="1.99",
     description="WholeTale Girder Volume Manager",
     author="Kacper Kowalik",
     author_email="xarthisius.kk@gmail.com",
